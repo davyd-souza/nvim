2dfab207eeb1769a76243a101e7cbc553bc6495a
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-10-21 17:59:58 -0300

    feat: implement todo comments plugin

--- lua/davyd-souza/plugins/todo-comments.lua
+++ lua/davyd-souza/plugins/todo-comments.lua
@@ -1,10 +1,9 @@
 return {
+	-- TODO: fix this
 	'folke/todo-comments.nvim',
-	event = 'VimEnter',
+	-- event = 'VimEnter',
 	dependencies = {
 		'nvim-lua/plenary.nvim'
 	},
-	opts = {
-		signs = false
-	},
+	opts = {},
 }
