2e434527b786c6165905d71bf2b46b09aabe26ae
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-10-20 21:21:19 -0300

    style: change from spaces to tabs

--- lua/davyd-souza/plugins/cloak.lua
+++ lua/davyd-souza/plugins/cloak.lua
@@ -1,17 +1,17 @@
 return {
-    'laytan/cloak.nvim',
-    config = function()
-        require('cloak').setup({
-            enabled = true,
-            cloak_character = "*",
-            highlight_group = "Comment",
-            patterns = {
-                file_pattern = {
-                    ".env*",
-                },
+	'laytan/cloak.nvim',
+	config = function()
+		require('cloak').setup({
+			enabled = true,
+			cloak_character = "*",
+			highlight_group = "Comment",
+			patterns = {
+				file_pattern = {
+					".env*",
+				},
 
-                cloak_pattern = "=.+"
-            }
-        })
-    end
+				cloak_pattern = "=.+"
+			}
+		})
+	end
 }
