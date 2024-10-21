6577b08ea23bfaac5c6436dae32bc02cc720b122
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-10-21 12:19:38 -0300

    feat: implement treesitter

---
+++ lua/davyd-souza/plugins/treesitter.lua
@@ -0,0 +1,49 @@
+return {
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+	config = function()
+		require('nvim-treesitter.configs').setup({
+			ensure_installed = {
+				'lua',
+				'vim',
+				'vimdoc',
+				'bash',
+				'css',
+				'html',
+				'javascript',
+				'lua',
+				'luadoc',
+				'typescript',
+			},
+
+			sync_install = false,
+
+			auto_install = true,
+
+			ignore_install = {},
+
+			modules = {},
+
+			indent = {
+				enable = true,
+			},
+
+			hightlight = {
+				enable = true,
+
+				additional_vim_regex_highlighting = false,
+			},
+		})
+
+		local treesitter_parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+		treesitter_parser_config.templ = {
+			install_info = {
+				url = 'https://github.com/vrischmann/tree-sitter-templ.git',
+				files = { 'src/parser.c', 'src/scanner.c' },
+				branch = 'master',
+			},
+		}
+
+		vim.treesitter.language.register('templ', 'templ')
+	end
+}
