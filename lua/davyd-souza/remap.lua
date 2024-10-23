a67c17e969b525bc87e16ee9fd30a0b389ebc7dc
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-10-23 11:57:12 -0300

    feat: implement remaps to add new line below and above

--- lua/davyd-souza/remap.lua
+++ lua/davyd-souza/remap.lua
@@ -31,3 +31,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>o", "m`o<Esc>``")
+vim.keymap.set("n", "<leader>O", "m`O<Esc>``")
