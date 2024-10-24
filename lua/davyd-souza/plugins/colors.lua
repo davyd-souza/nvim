58c4b603cd2d15ad48a2264dd99b03638e3a8d6e
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-10-24 15:08:43 -0300

    feat: add transparent background

--- lua/davyd-souza/plugins/colors.lua
+++ lua/davyd-souza/plugins/colors.lua
@@ -4,7 +4,10 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme tokyonight-night]])
+			vim.cmd([[colorscheme tokyonight]])
+
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
 	}
 }
