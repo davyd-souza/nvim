591e622f3d132896f75bb95749ea9d4f08c8f063
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-11-06 08:14:11 -0300

    feat: implement rose-pine theme

--- lua/davyd-souza/plugins/colors.lua
+++ lua/davyd-souza/plugins/colors.lua
@@ -1,3 +1,12 @@
+function ColorMyPencils(theme)
+	theme = theme or 'tokyonight-night'
+
+	vim.cmd.colorscheme(theme)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
 	{
 		"folke/tokyonight.nvim",
@@ -5,13 +14,26 @@ return {
 		priority = 1000,
 		config = function()
 			require("tokyonight").setup({
-				transparent = true
+				transparent = true,
+
+				on_colors = function(colors) end,
+
+				on_highlights = function(highlights, colors) end,
+			})
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require('rose-pine').setup({
+				disable_background = true,
+				styles = {
+					italic = false,
+				},
 			})
 
-			vim.cmd([[colorscheme tokyonight-night]])
-
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			ColorMyPencils('rose-pine')
 		end,
 	}
 }
