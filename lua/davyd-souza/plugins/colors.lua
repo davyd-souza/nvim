706d2eaf9426c96dc7864ba3d6c0e047ce6cac73
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-11-23 14:34:03 -0300

    feat: implement catppuccin theme

--- lua/davyd-souza/plugins/colors.lua
+++ lua/davyd-souza/plugins/colors.lua
@@ -33,7 +33,20 @@ return {
 				},
 			})
 
-			ColorMyPencils('rose-pine')
+			ColorMyPencils()
 		end,
-	}
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = true,
+			})
+
+			ColorMyPencils("catppuccin")
+		end
+	},
 }
