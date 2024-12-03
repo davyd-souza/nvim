24eceeff7b1554cb3b3d4b95c852c89da73ab5ce
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-12-03 18:55:21 -0300

    feat: implement trouble plugin

---
+++ lua/davyd-souza/plugins/trouble.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/trouble.nvim",
+	config = function()
+		require('trouble').setup({})
+
+		vim.keymap.set('n', '<leader>td', '<cmd>Trouble diagnostics toggle focus=false filter.buf=0<CR>', { desc = '[T]oggle Trouble [D]iagnostics Current Buffer' })
+
+		vim.keymap.set('n', '<leader>tdp', '<cmd>Trouble diagnostics toggle<CR>', { desc = '[T]oggle Trouble [D]iagnostics Project' })
+
+		vim.keymap.set('n', '<leader>tn', '<cmd> Trouble diagnostics next<CR>')
+
+		vim.keymap.set('n', '<leader>tp', '<cmd> Trouble diagnostics prev<CR>')
+	end
+}
